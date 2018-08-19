93703c6515443ce7a3e18661f83e024680a74d9e
mastas3 <rpsm90@gmail.com>
2018-08-19 12:45:51 +0300

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Link from 'next/link'
+
+const Home = () => (
+  <div>
+    <div className="hero">
+      <h1 className="title">Welcome to SmartImg!</h1>
+      <p className="description">
+        To get started, upload an image.
+      </p>
+
+      <div className="row">
+      </div>
+    </div>
+
+    <style jsx>{`
+
+    `}</style>
+  </div>
+)
+
+export default Home
